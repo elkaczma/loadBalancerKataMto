bf52ce0438217c7b923cbdf5bf764289f1e6bcee
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-06-13 11:34:30 +0200

    test6 green

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -1,19 +1,30 @@
 package edu.iis.mto.serverloadbalancer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServerLoadBalancer {
 
 	private static final double MAXIMUM_LOAD = 100.0d;
 
 	public void balance(Server[] servers, Vm[] vms) {
 		for (Vm vm : vms) {
-			Server lessLoaded = extractLessLoadedServer(servers);
-			lessLoaded.addVm(vm);
+			List<Server> capableServers = new ArrayList<Server>();
+			for (Server server : servers) {
+				if (server.canFit(vm)) {
+					capableServers.add(server);
+				}
+			}
+			Server lessLoaded = extractLessLoadedServer(capableServers);
+			if (lessLoaded!=null) {
+				lessLoaded.addVm(vm);
+			}
 		}
 	}
 
-	private Server extractLessLoadedServer(Server[] servers) {
+	private Server extractLessLoadedServer(List<Server> capableServers) {
 		Server lessLoaded = null;
-		for (Server server : servers) {
+		for (Server server : capableServers) {
 			if (lessLoaded == null || lessLoaded.currentLoadPercentage > server.currentLoadPercentage) {
 				lessLoaded = server;
 			}
